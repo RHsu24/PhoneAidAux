dee53f208326ebf56eea4c0b8ab0dfe9dda035e2
RHsu24 <93966876+RHsu24@users.noreply.github.com>
2026-04-21 11:54:46 +1000

    Add files via upload

--- speech_segment.py
+++ speech_segment.py
@@ -2,9 +2,9 @@ import csv
 import os
 import glob
 import wave
-import numpy as np
 import math
 from pathlib import Path
+import argparse
 
 def truncate_name(namelist):
     new_name = [0 for i in range(len(namelist))]
@@ -33,7 +33,7 @@ def make_dir(directory_name):
 def wav_manip(wav_file, dict, namelist):
     word_count = 1
     new_dir = namelist.split("_",1)
-    write_folder = Path("C:/Users/ricky/Desktop/Jobs/UCanSay66/First Week/Task1/Task1/PhonAid_to_be_scored/PhonAid_to_be_scored")
+    write_folder = Path.cwd()
     id_path = write_folder / new_dir[0]
     try: 
         with wave.open(wav_file,'rb') as wav_file:
@@ -45,16 +45,12 @@ def wav_manip(wav_file, dict, namelist):
             channels = wav_file.getnchannels()  # Mono or Stereo
             sample_width = wav_file.getsampwidth()  # Bytes
             frame_rate = wav_file.getframerate()    # Sampling Frequency
-            frames_tot = wav_file.getnframes()
             comp_type = wav_file.getcomptype()
             comp_name = wav_file.getcompname()
-            t_sec = wav_file.getnframes()/wav_file.getframerate()
-
-            
 
             for row in dict:
                 txt_count =  f"{word_count:03d}"
-                new_wav_name = str(txt_count) + '_' + row['text'] + '.wav'
+                new_wav_name = row['text'] + '_' + str(txt_count) + '.wav'
                 t_start =   float(row['tmin'])
                 t_end = float(row['tmax'])
                 # print(f'{new_wav_name}, {t_start}, {t_end}')
@@ -68,7 +64,7 @@ def wav_manip(wav_file, dict, namelist):
                 # Prepare to write to specific directory with patient ID
                 write_path = id_path / new_wav_name
 
-                print("new Write path success:,")
+                #print("new Write path success:,")
 
                 with wave.open(str(write_path),'wb') as wav_word:
                     wav_word.setparams((channels, sample_width, frame_rate, word_frames_tot,comp_type,comp_name))
@@ -83,8 +79,6 @@ def wav_manip(wav_file, dict, namelist):
         print(f"An unexpected error occurred: {e}")
     return None
 
-
-
 def append_filename(namelist):
     wav_name = [0 for i in range(len(namelist))]
     for n in range(len(namelist)):
@@ -92,12 +86,23 @@ def append_filename(namelist):
     #print(wav_name)
     return wav_name
 
-
 def main():
+    cwd = Path.cwd()
+    rw_folder = cwd / 'task1'
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_dir", type=str, default=rw_folder,
+                        help="Relative file path to the .wav and annotation files.")
+    args = parser.parse_args()
 
-    file_dir = r'C:\Users\ricky\Desktop\Jobs\UCanSay66\First Week\Task1\Task1\PhonAid_to_be_scored\PhonAid_to_be_scored' # this can be changed to user input
-    os.chdir(file_dir)
-
+    rd_folder = cwd / args.data_dir
+    print(rd_folder)
+    # Output dir
+    if not os.path.exists(rd_folder):
+        os.chdir(rw_folder)
+        print(rw_folder)
+    else:
+        os.chdir(rd_folder)
+ 
     # Find all files of csv type
     labfiles = []
     for files in glob.glob("*.csv"):
@@ -105,18 +110,19 @@ def main():
     
     if len(labfiles) == 0:
         print('There are no files with these extensions')
-    #else:
-        #print(labfiles)
+    # else:
+    #     print(labfiles)
         #new_labfiles = truncate_labfile(labfiles)
 
     namelist = glob.glob("*.csv")
     # Truncate the filename(s) to access .wav file
 
     set_name = truncate_name(namelist)
-    #print(set_name)
+    
     
     # try get row number
     for n in range(len(set_name)):
+        #print(set_name)
         if set_name[n] != 0:
             wav_name = (set_name[n] + '.wav')
             #print(wav_name)
